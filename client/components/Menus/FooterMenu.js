8ed0280fec2a201fc9565667b1eded98ceedacc7
vwyj <vanessa.wang@ymail.com>
2023-11-12 16:56:38 +0800

    PostScreen

--- client/components/Menus/FooterMenu.js
+++ client/components/Menus/FooterMenu.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
--- client/screens/Post.js
+++ client/screens/Post.js
@@ -1,11 +1,72 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React, {useContext} from 'react';
-import { AuthContext } from '../context/authContext';
+import { View, Text, StyleSheet, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
 import FooterMenu from '../components/Menus/FooterMenu';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import axios from 'axios';
 
-const Post = () => {
+const Post = ({navigation}) => {
+    // Local State
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    // Handle form data post DATA
+    const  handlePost = async () => {
+        try
+        {
+            setLoading(true);
+            if (!title)
+            {
+                alert("Please add Post Title");
+            }
+            if (!description)
+            {
+                alert("Please add Post Description");
+            }
+            const {data} = await axios.post("/post/create-post", {title, description});
+            setLoading(false);
+            alert(data?.message);
+            navigation.navigate("Home");
+        }
+        catch (error)
+        {
+            alert(error.response.data.message || error.message);
+            setLoading(false);
+            console.log(error);
+        }
+    }
+
     return (
-        <View style = { styles.container }>
+        <View style={ styles.container }>
+            <ScrollView>
+                <View style={{alignItems: "center"}}>
+                    <Text style={styles.header}>Create a Post</Text>
+                    <TextInput style={styles.inputBox} 
+                        placeholder="Add Post Title"
+                        placeholderTextColor={"gray"}
+                        value={title}
+                        onChangeText={(text) => setTitle(text)}
+                    />
+
+                    <TextInput style={styles.inputBox} 
+                        placeholder="Add Post Description"
+                        placeholderTextColor={"gray"}
+                        multiline={true}
+                        numberOfLines={6}
+                        value={description}
+                        onChangeText={(text) => setDescription(text)}
+                    />
+                </View>
+
+                <View style={{ alignItems: "center" }}>
+                    <TouchableOpacity style={styles.postBtn} onPress={handlePost}>
+                        <Text style={styles.postBtnText}>
+                        <FontAwesome5 name="plus-square" size={18} /> {" "}
+                        Create Post
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
             <View style = {{ flex: 1, justifyContent: "flex-end" }}>
                 <FooterMenu/>
             </View>  
@@ -20,7 +81,42 @@ const styles = StyleSheet.create({
         margin: 10,
         justifyContent: "space-between",
         marginTop: 40,
-    }
+    },
+    header:
+    {
+        fontSize: 25,
+        fontWeight: "bold",
+        textTransform: "uppercase",
+    },
+    inputBox:
+    {
+        backgroundColor: "#ffffff",
+        textAlignVertical:"top",
+        paddingTop: 10,
+        width: 320,
+        marginTop: 30,
+        fontSize: 16,
+        paddingLeft: 15,
+        borderColor: "gray",
+        borderWidth: 1,
+    },
+    postBtn:
+    {
+        backgroundColor: "black",
+        width: 300,
+        marginTop: 30,
+        height: 40,
+        borderRadius: 5,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 10,
+    },
+    postBtnText: 
+    {
+        color: "#ffffff",
+        fontSize: 18,
+        fontWeight: "bold",
+    },
 });
 
 
